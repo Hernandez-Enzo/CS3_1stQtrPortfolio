c5c636292aa225bd5f9f67580fd8da14785a3ae3
Hernandez-Enzo <b2029ejahernandez@pshs.edu.ph>
2025-08-06 12:08:21 +0800

    sample

---
+++ public/script.js
@@ -0,0 +1,15 @@
+let x;
+x = 100;
+x = "Hi"; 
+window.alert("Welcome to\nWeb Development")
+document.write("Welcome to<br>Web Development")
+console.log("Welcome to\nWeb Development")
+
+let age;
+age = 14;
+let maxage;
+maxage = 76;
+let numperday;
+numperday = 4;
+let totalrequired
+window.alert()
