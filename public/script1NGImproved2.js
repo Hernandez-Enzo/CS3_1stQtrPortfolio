13afecf6c5dd573e69f4ac527b2e4e5a159bf61e
Hernandez-Enzo <b2029ejahernandez@pshs.edu.ph>
2025-08-14 10:48:28 +0800

    partial finish

--- public/script1NGImproved2.js
+++ public/script1NGImproved2.js
@@ -1,10 +1,7 @@
-let sure = window.confirm("Sure of your numbers?")
-if(sure){
-let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge) - Number(age));
-document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
-alert(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge}`);
-console.log("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
-}
-else {
-    window.alert("Please click run again")
+function checkresultHere() {
+    let dage = document.getElementById("age").value;
+    let dmaxAge = document.getElementById("maxAge").value;
+    let dnumPerDay = document.getElementById("numPerDay").value;
+    let dresultHere = (dnumPerDay * 365) * (dmaxAge - dage)
+    document.getElementById("resultHere").innerHTML = `<b> You will need  + ${dresultHere}  to last you until the ripe age of  + ${dmaxAge} </b>`;
 }
