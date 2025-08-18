2b6d2f4d5a98172dd90a7123785d5a1147d85b54
Hernandez-Enzo <b2029ejahernandez@pshs.edu.ph>
2025-08-18 09:57:35 +0800

    finish 1NG Improved2

--- public/script1NGImproved2.js
+++ public/script1NGImproved2.js
@@ -3,5 +3,5 @@ function checkresultHere() {
     let dmaxAge = document.getElementById("maxAge").value;
     let dnumPerDay = document.getElementById("numPerDay").value;
     let dresultHere = (dnumPerDay * 365) * (dmaxAge - dage)
-    document.getElementById("resultHere").innerHTML = `<b> You will need  + ${dresultHere}  to last you until the ripe age of  + ${dmaxAge} </b>`;
+    document.getElementById("resultHere").innerHTML = `<b> You will need  ${dresultHere}  to last you until the ripe age of  ${dmaxAge} </b>`;
 }
